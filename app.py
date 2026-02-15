5b38b34985651142a4abb16ca430b3adbcd69670
Deon C J <aletjose067@gmail.com>
2026-02-15 17:38:34 +0530

    fix port binding for render

--- app.py
+++ app.py
@@ -61,5 +61,9 @@ def get_clip():
     else:
         return "No clip found"
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
